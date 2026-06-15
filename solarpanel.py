ebd30462ee92feee84cbfea906bcd4688807c777
moritz <moritz@kroepelin.net>
2026-06-15 09:22:30 +0200

    Added Solarpanel

---
+++ solarpanel.py
@@ -0,0 +1,15 @@
+from producer import Producer
+
+class SolarPanel(Producer):
+    """Produces energy when the sun is shining."""
+
+    def __init__(self, name: str, max_capacity: int):
+        super().__init__(name, max_capacity)
+
+    def get_current_capacity(self, weather: float, hour: int):
+        """Returns current power production by solar panel"""
+
+        if 6 <= hour <= 18:
+            return weather * self.max_capacity
+
+        return 0
