aafdf9fefdccdf90989b7e299f343ef7f57427b4
moritz <moritz@kroepelin.net>
2026-06-25 13:00:58 +0200

    updated producer to Class concept

--- src/producer.py
+++ src/producer.py
@@ -1,14 +1,24 @@
+from abc import ABC, abstractmethod
+
 from .energy_asset import EnergyAsset
 
 
-class Producer(EnergyAsset):
+class Producer(EnergyAsset, ABC):
     """Parent class for all power producers"""
 
-    def __init__(self, name: str, max_capacity: float):
+    def __init__(self, name: str, max_capacity: float, efficiency: float):
         super().__init__(name)
         self.max_capacity = max_capacity # in kW
+        self.efficiency = efficiency
 
-    def get_current_capacity(self, weather: float, hour: int) -> float:
+    def update(self, current_hour: int, weather_data: dict) -> float:
         """Returns current power production"""
-        raise NotImplementedError("Class must be implemented by Subclass")
+        if self.is_connected:
+            return self.produce(current_hour, weather_data)
+        return 0
 
+
+    @abstractmethod
+    def produce(self, current_hour: int, weather_data: dict) -> float:
+        """Returns current power production"""
+        pass
