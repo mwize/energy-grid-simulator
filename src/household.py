18c0c5da58ee587e4ab628d6aea8cc69ceac28c9
moritz <moritz@kroepelin.net>
2026-06-25 14:02:50 +0200

    Fixed attribute in household.py

--- src/household.py
+++ src/household.py
@@ -7,6 +7,7 @@ class HouseHold(ABC, Consumer):
     """"""
 
     def __init__(self, asset_id: int, num_residents: int, priority: int):
+        self.num_residents = num_residents
         demand = num_residents * 1000 # kW
         consumption_profile = [demand]# tbd
         super().__init__("Household", demand, consumption_profile, priority, asset_id)
