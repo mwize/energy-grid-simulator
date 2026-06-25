82fef648b41c26cb6497adafcf8799a84728297f
moritz <moritz@kroepelin.net>
2026-06-25 13:41:16 +0200

    Added household class

---
+++ src/household.py
@@ -0,0 +1,16 @@
+from abc import ABC
+
+from consumer import Consumer
+
+
+class HouseHold(ABC, Consumer):
+    """"""
+
+    def __init__(self, asset_id: int, num_residents: int, priority: int):
+        demand = num_residents * 1000 # kW
+        consumption_profile = [demand]# tbd
+        super().__init__("Household", demand, consumption_profile, priority, asset_id)
+
+    def consume(self, current_hour) -> float:
+        """"""
+        return self.consumption_profile[current_hour]
