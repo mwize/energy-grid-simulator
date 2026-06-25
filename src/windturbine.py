f2f3db9eb64d505fbb9f452a3b531011a89be76c
moritz <moritz@kroepelin.net>
2026-06-25 13:14:09 +0200

    Added Wind Turbine class

---
+++ src/windturbine.py
@@ -0,0 +1,13 @@
+from abc import ABC
+
+from producer import Producer
+
+
+class WindTurbine(Producer, ABC):
+    """Produces energy when the wind is blowing."""
+
+    def __init__(self, max_capacity: float, asset_id: int):
+        super().__init__("Wind Turbine", max_capacity, 1, asset_id) # may have to be changed
+
+    def produce(self, current_hour: int, weather_data: dict) -> float:
+        pass
