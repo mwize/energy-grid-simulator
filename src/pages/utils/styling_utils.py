a78960a4aa9acbdbcf83466d7040ec4da6f4d4c4
moritz <moritz@kroepelin.net>
2026-07-01 18:15:11 +0200

    added styling_utils for css styling possiblities

---
+++ src/pages/utils/styling_utils.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+def inject_custom_css():
+    # Moves Buttons to the bottom
+    st.markdown("""
+        <style>
+        div[data-testid="stVerticalBlock"][height="430px"] {
+            display: flex !important;
+            flex-direction: column !important;
+        }
+
+        div[data-testid="stVerticalBlock"][height="430px"] > div:last-child {
+            margin-top: auto !important;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+    st.markdown("""
+                <style>
+                    .block-container {
+                        padding-top: 60px;
+                        padding-bottom: 60px;
+                        padding-left: 10px !important;
+                        padding-right: 10px !important;
+                    }
+                </style>
+            """, unsafe_allow_html=True)
+
+inject_custom_css()
