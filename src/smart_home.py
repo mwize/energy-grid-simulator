29c783e606d0fcb13a58d199517873d15281bfc1
moritz <moritz@kroepelin.net>
2026-07-01 11:23:47 +0200

    Updated UML chart

--- src/smart_home.py
+++ src/smart_home.py
@@ -1,4 +1,4 @@
-from household import HouseHold
+from .household import HouseHold
 from .consumer import Consumer
 from .producer import Producer
 
@@ -7,7 +7,6 @@ class SmartHome(HouseHold, Producer):
     def __init__(self, num_residents: int, asset_id: int, priority: int):
         HouseHold.__init__(self, asset_id, num_residents, priority)
         Producer.__init__(self, 100, 1, asset_id)
-        self.num_residents = num_residents
     def produce(self, current_hour: int, weather_data: dict) -> float:
         pass
     def consume(self, current_hour: int) -> float:
