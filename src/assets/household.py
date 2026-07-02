7c3d1a59aea64c328d1b98fe6fb89be4a5ce06ce
moritz <moritz@kroepelin.net>
2026-07-02 09:54:43 +0200

    Fixed Household card consumption indicator

--- src/assets/household.py
+++ src/assets/household.py
@@ -7,11 +7,13 @@ class HouseHold(Consumer):
     def __init__(self, asset_id: int = None, num_residents: int = 1, priority: int = 0, name: str = "Household", personal_demand: float = 1.5):
         """initializes a household with a number of residents, a personal demand per resident, and a standard consumption profile."""
         self.num_residents = num_residents
+        self.personal_demand = personal_demand
         demand = num_residents * personal_demand
         consumption_profile = [0,0,0,0,0,1,2,2,1,1,1,2,2,1,1,0,1,3,3,2,1,1,1,0]
         super().__init__(name = name, peak_power_demand=demand, consumption_profile=consumption_profile, priority=priority, asset_id=asset_id)
 
     def consume(self, current_hour) -> float:
         """Returns the consumption of the household at a given hour based on the consumption profile."""
-        return -self.consumption_profile[current_hour % 24] * self.num_residents
+
+        return -self.consumption_profile[current_hour % 24] * self.num_residents*self.personal_demand
     
