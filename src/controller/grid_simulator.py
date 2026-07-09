f277a3e079c63fc04135560c7ddad75772977e80
Florian Kunkel <florian.kunkel.2006@gmail.com>
2026-07-09 11:34:46 +0200

    fixed Current Power meter can go negative for 1 step, when battery capacity == 0 and demand is high, should be capped at 0 Fixes #27

--- src/controller/grid_simulator.py
+++ src/controller/grid_simulator.py
@@ -70,7 +70,7 @@ class GridSimulator:
         return self.battery_controller.store(net_balance)
 
     def overload_check(self, bal: float) -> bool:
-        """Checks whether the grid is currently overloaded and turns off all Assets if this is the case"""
+        """Checks whether the grid is currently overloaded and turns off all  Consiuming Assets if this is the case"""
         if bal < 0:
             for asset in self.grid_members:
                 if isinstance(asset, Consumer):
--- src/pages/main_screen.py
+++ src/pages/main_screen.py
@@ -148,7 +148,10 @@ def main_screen(assets: list[EnergyAsset], current_time: int, weather_data: dict
                 if a.is_connected:
                     total_kwh += a.update(current_time, weather_data)
 
-            st.metric("Current Net-Power", f"{total_kwh:.2f} kW")
+            actual_kW = f"{total_kwh:.2f} kW"
+            actual_kW = "0.00 kW" if st.session_state.grid_simulator.battery_controller.max_kwh == 0 and total_kwh <= 0 else actual_kW
+            
+            st.metric("Current Net-Power", actual_kW)
             st.divider()
 
             df = pd.DataFrame(st.session_state.grid_simulator.power_history).set_index("Time")
